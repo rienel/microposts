9f33532fa5da17af1ce706b3f490fa4ae4233266
nori <rienel1226@gmail.com>
2015-07-08 17:52:18 +0000

    KADAI: add search

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,13 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+
+  def self.search(search)
+    if search
+      Micropost.where(['content LIKE ?', "%#{search}%"])
+    else
+      Micropost.all #全て表示。
+    end
+  end
+
 end
