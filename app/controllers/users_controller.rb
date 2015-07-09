9f54725e7d1392e5dde477ff52271f2b51d5778d
nori <rienel1226@gmail.com>
2015-07-09 14:34:02 +0000

    add kaminari paging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show # 追加
     @user = User.find(params[:id])
-    @microposts = @user.microposts
+    @microposts = @user.microposts.page(params[:page]).per(5)
   end
   
   def new
